679bbbd035b521ba6dca362a416843aece952804
Sem Uytterhoeven <sem.uytterhoeven@archimiddle.com>
2015-11-17 12:25:12 +0100

    Submission of the course project.

---
+++ README.md
@@ -0,0 +1,26 @@
+Getting and Cleaning Data: Course Project
+=========================================
+
+Description run_analysis.R
+--------------------------
+
+The goal of this script is to prepare tidy data that can be used for later analysis. It performs the following actions:
+
+- The script assumes that all data is contained in a folder named "UCI HAR Dataset" containing two folders named "train" and "test".
+- The script reads in all raw data and merges similar data contained in the training and test folders using the rbind() function.
+- Unnecessary columns, as specified in the requirements, are dropped. Correct, comprehensable column names are added. 
+- The labels for the activities performed while gathering the measurements are added in integer format as well as string format. 
+- The IDs for the subjects performing the measurements are added.
+- After the full dataset is created as specified in the requirements, we reshape the data using the melt() and dcast() functions to display the average for each variable for each activity and each subject. 
+- This tidy data is writen out to "tidy_data.txt".
+
+Annotations for what the functions do can be found in the script, to make reading the code easier.
+
+
+Files
+-----
+
+- **README.md**: the readme file for this repo.
+- **CodeBook.md**: codebook containing the description for the variables used in the script.
+- **run_analysis.R**: script for the creation of tidy_data.txt.
+- **tidy_data.txt**: output of run_analysis.R.
---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+# Install and load the necessary packages
+if (!require("reshape2")) { install.packages("reshape2") }
+if (!require("dplyr")) { install.packages("dplyr") }
+library("reshape2")
+library("dplyr")
+
+# Read in the training and test set raw data files.
+X_train <- read.table("UCI\ HAR\ Dataset/train/X_train.txt")
+X_test <- read.table("UCI\ HAR\ Dataset/test/X_test.txt")
+
+# Read in the features data
+features <- read.table("UCI\ HAR\ Dataset/features.txt")[,2]
+
+# Merge data from X_train with the data from X_test
+X_full <- rbind(X_train, X_test)
+
+# Set the appropriate column names on our merged set
+names(X_full) <- features
+
+# Extract all columns containing mean and std
+X_full <- X_full[, grep('mean|std', colnames(X_full))]
+# Drop all columns containing meanFreq
+X_full <- X_full[, - grep('meanFreq', colnames(X_full))]
+
+# Read in the labels for the training and test sets
+y_train <- read.table("UCI\ HAR\ Dataset/train/y_train.txt", col.names = c("ActivityID"))
+y_test <- read.table("UCI\ HAR\ Dataset/test/y_test.txt", col.names = c("ActivityID"))
+
+# Combine the data from y_train with the data from y_test in one dataframe
+y_full <- rbind(y_train, y_test)
+
+# Read in the activity labels data and replace them with the integer values
+act_labels <- read.table("UCI\ HAR\ Dataset/activity_labels.txt", col.names = c("ActivityID", "ActivityLabel"))
+y_full <- left_join(y_full, act_labels, by = "ActivityID")
+
+# Combine the labels with the features
+X_y_full <- cbind(X_full, y_full)
+
+# Read the subjects file and combine the two data
+subject_train <- read.table("UCI\ HAR\ Dataset/train/subject_train.txt" , col.names = c("SubjectID"))
+subject_test <- read.table("UCI\ HAR\ Dataset/test/subject_test.txt" , col.names = c("SubjectID"))
+subject_full <- rbind(subject_train, subject_test)
+
+# Add the subject column to X_y_full
+X_y_full$SubjectID <- subject_full$SubjectID
+
+# Melt the X_y_full dataframe
+allIDs <- c('SubjectID', 'ActivityID', 'ActivityLabel')
+allVariables <- setdiff(colnames(X_y_full), allIDs)
+X_y_fullMelt <-melt(X_y_full, id = allIDs, measure.vars = allVariables)
+
+# Create a seperate data set with the average of each variable for each activity and each subject.
+tidy_data = dcast(X_y_fullMelt, SubjectID + ActivityLabel ~ variable, mean)
+
+# Edit the column names to reflect that they hold the means
+names(tidy_data) <- c('SubjectID', 'ActivityLabel', paste("Avg", setdiff(colnames(tidy_data), c('SubjectID', 'ActivityLabel')), sep = ""))
+
+ # Write out the tidy dataset in txt format, not using column names
+write.table(tidy_data, "tidy_data.txt", row.name=FALSE)
